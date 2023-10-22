d439dbfbfab441e1ff9b6321aaa600308d565e80
Veronika-Kitsul <veronika.kitsul@gmail.com>
2023-10-22 05:26:15 -0400

    sending codes to the backend

--- super.sh
+++ super.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# post temporary code to the website
+curl -X POST -H "Content-Type: application/json" -d '{"code":"200", "video":"temp"}' http://localhost:3001
+
 # Run the Python script
 python3 generate_script.py
 
@@ -12,3 +15,6 @@ python3 narration.py
 
 # Link it all together 
 python3 linker.py
+
+# post the video
+curl -X POST -H "Content-Type: application/json" -d '{"code":"200", "video":"final.mp4"}' http://localhost:3001
